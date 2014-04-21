786ab3436006ec38142e863f393a416621822979
TubeScreamer <crazyasss@gmail.com>
2014-04-21 11:38:42 +0300

    added ui_elements activity and changed the app icon

--- BetterQApp/gen/cto/lead/betterqapp/R.java
+++ BetterQApp/gen/cto/lead/betterqapp/R.java
@@ -22,10 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int old_ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080011;
-        public static final int action_settings=0x7f080014;
+        public static final int action_settings=0x7f08001b;
         public static final int analogClock1=0x7f080001;
         public static final int bandName=0x7f080004;
         public static final int button1=0x7f08000a;
@@ -33,18 +34,25 @@ public final class R {
         public static final int buttonList=0x7f08000e;
         public static final int buttonMap=0x7f08000f;
         public static final int buttonUI=0x7f080010;
+        public static final int choice1=0x7f08001a;
+        public static final int choice2=0x7f080018;
+        public static final int choice3=0x7f080019;
         public static final int digitalClock1=0x7f080003;
         public static final int editText1=0x7f080007;
         public static final int editText2=0x7f080009;
         public static final int listBands=0x7f080006;
         public static final int location=0x7f080012;
         public static final int mapButton=0x7f080013;
+        public static final int radioGroup=0x7f080017;
         public static final int searchBands=0x7f080005;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
         public static final int textView3=0x7f080008;
         public static final int textView4=0x7f08000b;
         public static final int textView5=0x7f08000c;
+        public static final int ui_button=0x7f080015;
+        public static final int ui_checkbox=0x7f080014;
+        public static final int ui_textView=0x7f080016;
     }
     public static final class layout {
         public static final int clock_screen=0x7f030000;
@@ -53,6 +61,7 @@ public final class R {
         public static final int login_screen=0x7f030003;
         public static final int main_screen=0x7f030004;
         public static final int map_screen=0x7f030005;
+        public static final int ui_elements_screen=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -62,14 +71,21 @@ public final class R {
         public static final int analog_clock=0x7f05000b;
         public static final int app_name=0x7f050000;
         public static final int attempts=0x7f050006;
+        public static final int check_box_not_checked=0x7f050011;
+        public static final int checkbox_string=0x7f050010;
+        public static final int choice_text1=0x7f050014;
+        public static final int choice_text2=0x7f050015;
+        public static final int choice_text3=0x7f050016;
         public static final int clock=0x7f05000a;
         public static final int digital_clock=0x7f05000c;
         public static final int hello_world=0x7f050002;
+        public static final int hide_checkbox=0x7f050012;
         public static final int list=0x7f050009;
         public static final int location_string=0x7f05000e;
         public static final int login=0x7f050005;
         public static final int map=0x7f050008;
         public static final int password=0x7f050004;
+        public static final int radio_no_choice=0x7f050013;
         public static final int search_text=0x7f05000f;
         public static final int show_map_string=0x7f05000d;
         public static final int ui=0x7f050007;
--- BetterQApp/src/cto/lead/betterqapp/UIElementsActivity.java
+++ BetterQApp/src/cto/lead/betterqapp/UIElementsActivity.java
@@ -1,16 +1,85 @@
 package cto.lead.betterqapp;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 /*
- *  slider
- *  rate
- *  check boxes
- *  combo boxes
- *  radio buttons
+ *  check boxes				// UICheckBox
+ *  combo boxes 			// UISpinner 
+ *  radio buttons			// UIRadioGroup
+ *  rate					// UIRatingsBar
  *  pictures
+ *  slider
  */
 public class UIElementsActivity extends Activity 
 {
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ui_elements_screen);
+		
+		/*
+		 * Check box
+		 */
+		final CheckBox checkbox = (CheckBox) findViewById(R.id.ui_checkbox);
+        checkbox.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (checkbox.isChecked()) {
+					checkbox.setText("I'm checked");
+				} else {
+					checkbox.setText("I'm not checked");
+				}
+			}
+		});
+        
+        final Button button = (Button) findViewById(R.id.ui_button);
+        button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (checkbox.isShown()) {
+					checkbox.setVisibility(View.INVISIBLE);
+					button.setText("Unhide CheckBox");
+				} else {
+					checkbox.setVisibility(View.VISIBLE);
+					button.setText("Hide CheckBox");
+				}
+			}
+		});
+        
+        /*
+         * Radio
+         */
+        final TextView tv = (TextView) findViewById(R.id.ui_textView);
+
+		final OnClickListener radioListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				RadioButton rb = (RadioButton) v;
+				tv.setText(rb.getText() + " chosen");
+			}
+		};
+		final RadioButton choice1 = (RadioButton) findViewById(R.id.choice1);
+		choice1.setOnClickListener(radioListener);
+
+		final RadioButton choice2 = (RadioButton) findViewById(R.id.choice2);
+		choice2.setOnClickListener(radioListener);
+
+		final RadioButton choice3 = (RadioButton) findViewById(R.id.choice3);
+		choice3.setOnClickListener(radioListener);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
