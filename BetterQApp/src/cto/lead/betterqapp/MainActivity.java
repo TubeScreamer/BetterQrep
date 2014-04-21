e79fc7c1813d106ae6e089a1629efd58ff8118a4
TubeScreamer <crazyasss@gmail.com>
2014-04-21 16:31:45 +0300

    added register (without Server) functionality

--- BetterQApp/src/cto/lead/betterqapp/MainActivity.java
+++ BetterQApp/src/cto/lead/betterqapp/MainActivity.java
@@ -1,9 +1,10 @@
 package cto.lead.betterqapp;
 
+import java.util.HashMap;
+
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,39 +14,108 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/*
+ * ---------------------Login logic------------------------
+ * 0. Register 
+ * 	- new user enters name and password, that saved in DB (Server)
+ * 1. Login 
+ * 	- first time the user enters his name and password 
+ * 	- the app remembers his credentials (phone shared preferences) 
+ * 2. Next time the login window is skipped (the app takes all data from phone shared preferences)
+ * 3. No log out option (like WAZE, Whatsapp,...), only one account per phone
+ */
 public class MainActivity extends Activity {
 	private static final int MAX_ATTEMPTS = 3;
 	public static final String TAG = "BetterQApp";
+	private static final String USER_IS_LOGGED = "logged_flag";
+	private static final String LOGGED_USER_NAME = "logged_user_name";
 	
 	private EditText username;
 	private EditText password;
-	private TextView attempts;
 	private Button login;
 	private int counter;
+	private SharedPreferences prefs;
+	
+	private static final HashMap<String, String> userCredentials = new HashMap<String, String>() { {
+			put("nik", "zu");
+			put("alex", "du");
+			put("admin", "admin");
+			put("zero", "hero");
+		}
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_screen);
-		counter = MAX_ATTEMPTS;
-		username = (EditText)findViewById(R.id.editText1);
-		password = (EditText)findViewById(R.id.editText2);
-		attempts = (TextView)findViewById(R.id.textView5);
-		attempts.setText(Integer.toString(counter));
-		login = (Button)findViewById(R.id.button1);
+		
+		prefs  = getPreferences(MODE_PRIVATE);
+		if(!prefs.getBoolean(USER_IS_LOGGED, false)){ // second parameter?
+			setContentView(R.layout.login_screen);
+			initLoginData();
+		}
+		else{
+			setContentView(R.layout.main_screen);
+			showGreeting();
+		}
 	}
 
+	private void initLoginData(){
+		counter = MAX_ATTEMPTS;
+		username = (EditText)findViewById(R.id.etUsername);
+		password = (EditText)findViewById(R.id.etPassword);
+		login = (Button)findViewById(R.id.btnLogin);
+	}
+	
+	public void register(View view){
+		// check if the user is already exists
+		if(userExists()){
+			Toast.makeText(getApplicationContext(), "User Already Exists!", Toast.LENGTH_SHORT).show();
+		}
+		else {
+			// Register new user
+			// for now we'll use hashmap to store default users
+			// TODO call Server method to update DB - register
+			// for now, there is nothing to do
+		}
+	}
+	
+	private void showGreeting(){
+		TextView greeting = (TextView)findViewById(R.id.tvLoggedUser);
+		String loggedUser = prefs.getString(LOGGED_USER_NAME, "");
+		greeting.setText("Hello " + loggedUser + "!");
+	}
+	
+	private boolean userExists(){
+		// TODO make call to the Server instead of the code below
+		if(userCredentials.containsKey(username.getText().toString())){
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isLegalUser(){
+		// TODO make call to the Server instead of the code below
+		if(userExists() && userCredentials.get(username.getText().toString()).equals(password.getText().toString())){
+			return true;
+		}
+		return false;
+	}
+	
 	public void login(View view){	
-		if(username.getText().toString().equals("nik") && password.getText().toString().equals("zu")){
+		if(isLegalUser()){
 			Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
-			
+			// store in shared preferences
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putBoolean(USER_IS_LOGGED, true);
+			editor.putString(LOGGED_USER_NAME, username.getText().toString());
+			editor.commit();
+						
 			this.setContentView(R.layout.main_screen);
+			showGreeting();
 		}
 		else{
 			Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-			attempts.setBackgroundColor(Color.RED);
 			counter--;
-			attempts.setText(Integer.toString(counter));
 			if(counter <= 0){
 				login.setEnabled(false);
 			}
